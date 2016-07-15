a485cf317f52cefb6a716459fd5250d6f7b82719
Patric Eberle <patric.eberle@movento.com>
2016-07-15 14:00:07 +0200

    disabling import/no-unresolved due to too much maintenance work

--- rules/import.js
+++ rules/import.js
@@ -1,11 +1,6 @@
 module.exports = {
   'rules': {
     'import/no-mutable-exports': 1,
-    'import/no-unresolved': [1, {
-      'ignore': [
-        '.*?\/utils\/.*', // Ember util scripts
-        '.*?\/helpers\/.*'  // Ember helper scripts
-      ]
-    }]
+    'import/no-unresolved': 0
   }
 };
