3872106dee23f0b33df713c2211ea8a19fdb4664
Patric Eberle <patric.eberle@movento.com>
2017-09-27 16:54:27 +0200

    Fixes invalid paths

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
-    'eslint-config-movento/rules/best-practices',
-    'eslint-config-movento/rules/errors',
-    'eslint-config-movento/rules/es6',
-    'eslint-config-movento/rules/import',
-    'eslint-config-movento/rules/legacy',
-    'eslint-config-movento/rules/node',
-    'eslint-config-movento/rules/strict',
-    'eslint-config-movento/rules/style',
-    'eslint-config-movento/rules/variables',
-    'eslint-config-movento/settings/import_core-modules.js'
+    './rules/best-practices.js',
+    './rules/errors.js',
+    './rules/es6.js',
+    './rules/import.js',
+    './rules/legacy.js',
+    './rules/node.js',
+    './rules/strict.js',
+    './rules/style.js',
+    './rules/variables.js',
+    './settings/import_core-modules.js'
   ],
   env: {
     browser: true,
@@ -19,7 +19,6 @@ module.exports = {
     mocha: false,
     jasmine: false
   },
-  ecmaFeatures: {},
   globals: {},
   rules: {}
 };
