f7feb5fa45c7a0fbd5879ca3e28282b20f69a28a
Patric Eberle <patric.eberle@cec.valantic.com>
2018-04-12 11:37:53 +0200

    Updats comma-dangle to not enforce them in any way

--- rules/errors.js
+++ rules/errors.js
@@ -1,7 +1,8 @@
 module.exports = {
   rules: {
     // require or disallow trailing commas
-    'comma-dangle': [2, 'never'],
+    // https://eslint.org/docs/rules/comma-dangle
+    'comma-dangle': 0,
 
     // disallow use of constant expressions in conditions
     'no-constant-condition': 2,
