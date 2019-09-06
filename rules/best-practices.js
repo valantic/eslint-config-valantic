c0154b29c3ced99e797adf53d344e094b8782f36
Patric Eberle <patric.eberle@cec.valantic.com>
2019-09-06 16:22:50 +0200

    Updates packages to get rid of security issues.

--- rules/best-practices.js
+++ rules/best-practices.js
@@ -6,23 +6,23 @@ module.exports = {
 
     // Enforce consistent brace style for all control statements
     'curly': [2, 'all'],
-    
+
     // Enforce consistent newlines before and after dots
     'dot-location': [2, 'property'],
-    
+
     // Disallow reassignment of function parameters
     // Disallow parameter object manipulation
     // Rule: http://eslint.org/docs/rules/no-param-reassign.html
     // - Allowed to support default fallback: var foo = foo || 'baa';
     'no-param-reassign': 0,
-    
+
     // Disallow unmodified conditions of loops
     // http://eslint.org/docs/rules/no-unmodified-loop-condition
     'no-unmodified-loop-condition': 2,
-    
+
     // Disallow unnecessary .call() and .apply()
     'no-useless-call': 2,
-    
+
     // Disallow usage of configurable warning terms in comments.
     'no-warning-comments': [1, {
       terms: ['fixme', 'xxx', 'console', 'stupid', 'dumb', 'fuck']
