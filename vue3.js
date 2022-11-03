944093fc8d4321c1cf6702473eeb5de229c82758
Patric Eberle <patric.eberle@cec.valantic.com>
2022-11-03 11:40:39 +0100

    Refactors Vue 3 config to prevent a syntax error in combination with TS

--- vue3.js
+++ vue3.js
@@ -1,12 +1,14 @@
 module.exports = {
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
     './index.js',
     './rules/vue.js',
-    './typescript.js',
+    './rules/typescript.js',
   ],
   plugins: [
     'vue',
+    '@typescript-eslint/eslint-plugin',
   ],
   rules: {
     'import/extensions': ['warn', 'always'],
