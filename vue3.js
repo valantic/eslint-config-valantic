0a01d2459fbc79fe30308955adbbe8a900f1553a
Patric Eberle <patric.eberle@cec.valantic.com>
2022-11-03 10:17:59 +0100

    Changes order of rules import, because 'plugin:vue/vue3-recommended' enable 'indent' again

--- vue3.js
+++ vue3.js
@@ -1,9 +1,9 @@
 module.exports = {
   extends: [
+    'plugin:vue/vue3-recommended',
     './index.js',
     './rules/vue.js',
-    'plugin:vue/vue3-recommended',
-    '@vue/typescript',
+    './typescript.js',
   ],
   plugins: [
     'vue',
